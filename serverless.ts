07df32d8ac3175749d5db17dafea99c6ab3b383c
ThallesP <contato@thalles.me>
2022-10-06 22:30:15 -0300

    fix: build failing

--- serverless.ts
+++ serverless.ts
@@ -6,7 +6,11 @@ import graphQLHandler from "./src/handler";
 const serverlessConfiguration: AWS = {
   service: "serverless-graphql",
   frameworkVersion: "3",
-  plugins: ["serverless-plugin-typescript", "serverless-offline"],
+  plugins: [
+    "serverless-plugin-common-excludes",
+    "serverless-plugin-typescript",
+    "serverless-offline",
+  ],
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
@@ -17,36 +21,23 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
-      DATABASE_URL:
-        "postgres://ogrilo:serenata@${self:custom.dbHost}:${self:custom.dbPort}/todosdatabase",
     },
   },
   functions: { graphQLHandler },
   package: {
     individually: true,
-    patterns: ["node_modules/.prisma/client/libquery_engine-rhel-*"],
+    patterns: [
+      "prisma/schema.prisma",
+      "node_modules/.prisma",
+      "!node_modules/.prisma/client/libquery_engine-*",
+      "node_modules/.prisma/client/libquery_engine-rhel-*",
+      "!node_modules/prisma/libquery_engine-*",
+      "!node_modules/@prisma/engines/**",
+    ],
   },
   custom: {
-    dbHost: "!GetAtt TodosDatabase.Endpoint.Host",
-    dbPort: "!GetAtt TodosDatabase.Endpoint.Port",
     "serverless-offline": {
       host: "127.0.0.1",
-    },
-  },
-  resources: {
-    Resources: {
-      TodosDatabase: {
-        Type: "AWS::RDS::DBInstance",
-        Properties: {
-          MasterUsername: "ogrilo",
-          MasterUserPassword: "serenata",
-          DBInstanceClass: "db.t2.micro",
-          Engine: "postgres",
-          EngineVersion: "12",
-          AllocatedStorage: "5",
-          DBName: "todos",
-        },
-      },
     },
   },
 };
